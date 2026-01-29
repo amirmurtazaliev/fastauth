8a11571a8e1f08e54bd88937ddf8565639194554
Amir Murtazaliev <murtazalievamir11@gmail.com>
2026-01-29 19:02:44 +0300

    added Password work

--- auth-app/database.py
+++ auth-app/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from passlib.context import CryptContext
 from ..config import settings
 
 async_engine = create_async_engine(
@@ -16,7 +17,13 @@ def init_db():
     
 async def get_session():
     session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
+    yield session
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Password:
+    def get_password_hash(self, password: str) -> str:
+        return pwd_context.hash(password)
+    
+    def verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)
