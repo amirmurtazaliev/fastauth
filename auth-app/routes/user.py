e9b4b2502774d3d61ca5f65d0a509d2471a8f248
Amir Murtazaliev <murtazalievamir11@gmail.com>
2026-01-31 11:54:53 +0300

    renamed user router

--- auth-app/routes/user.py
+++ auth-app/routes/user.py
@@ -4,16 +4,16 @@ from ..database import get_session
 from ..schemas.user import UserBase, UserCreate
 from ..services.user_service import UserService
 
-router = APIRouter(
+user_router = APIRouter(
     prefix="/api/user",
     tags=["user"]
 )
 
-@router.post("/login")
+@user_router.post("/login")
 async def login_user(user: UserBase, db: AsyncSession = Depends(get_session)):
     ...
     
-@router.post("/register")
+@user_router.post("/register")
 async def register_user(user: UserBase, db: AsyncSession = Depends(get_session)):
     service = UserService(db)
     return service.create_user(user)
